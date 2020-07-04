3f1ecea740d72f2767c3f1e8d2de876c96ae4655
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-04 09:13:19 -0400

    Update pieces.js

--- js/pieces.js
+++ js/pieces.js
@@ -1,22 +1,56 @@
 class Piece{
-	constructor(){
-  	this.rot = null;
-     
-  }
+//specify the starting position and rotation(0,90,180,270) of the pyramid
+	constructor(startingRotation,coordX,coordY){
   
-  get rotation(){
-  	return this.rot;
-  }
-  
-  set rotation(degree){
+  	this.rot = startingRotation % 360;
+    this.coordX = coordX;
+    this.coordY = coordY;
     
-    if(degree % 90 === 0){
-    	degree %= 360;
-      this.rot = degree;
-    } 
+    //the true in the sides array represent the sides that can be hit with
+    //a laser without the piece being destroyed
+    this.sides = [true,false,false,true];
+    
+    //rotates the array to the right so the sides faced there starting rotation
+    for(let i = this.rot;i > 0;i-=90){
+    	this.sides.unshift(this.sides.pop());
+    }
+  }
+  //rotates the peice 90 degrees. If negative eqauls true , rotates the pyramid -90 degrees
+  rotate90deg(bNegative){
+  
+  	if(!bNegative){
+    //rotates the array to the right
+    	this.sides.unshift(this.sides.pop());
+      
+      this.rot = (this.rot + 90) % 360;
+    }
+    
+    else if(bNegative){
+    	//rotates the array to the left
+    	this.sides.push(this.sides.shift());
+      
+      this.rot = this.rot === 0 ? 270 : this.rot - 90;
+    }
+    
+    //console.log(this.rot);
+    //console.log(this.sides);
   }
   
-  isDestroyed(laserAngle){
   
+  get x(){
+  	return this.coordX;
   }
+  
+  get y(){
+  	return this.coordY;
+  }
+  
+  set x(increment){
+  	this.coordX += increment;
+  }
+  
+  set y(increment){
+  	this.coordY += increment;
+  }
+  
 }
