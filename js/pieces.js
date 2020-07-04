d20def726aeb67436899fea36a505c6d6094134d
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-04 09:20:20 -0400

    Update pieces.js

--- js/pieces.js
+++ js/pieces.js
@@ -1,56 +1,55 @@
 class Piece{
-//specify the starting position and rotation(0,90,180,270) of the pyramid
-	constructor(startingRotation,coordX,coordY){
+//specify the starting position, rotation(0,90,180,270) of the Piece.
+//the sides parameter is supposed to get an array of size 4 filled with booleans true or false that represent 
+//if a laser can hit that side without the piece getting destroyed.
+	constructor(startingRotation,coordX,coordY,sides){
   
-  	this.rot = startingRotation % 360;
-    this.coordX = coordX;
-    this.coordY = coordY;
+  		this.rot = startingRotation % 360;
+    		this.coordX = coordX;
+    		this.coordY = coordY;
     
-    //the true in the sides array represent the sides that can be hit with
-    //a laser without the piece being destroyed
-    this.sides = [true,false,false,true];
+    		//the true in the sides array represent the sides that can be hit with
+    		//a laser without the piece being destroyed
+    		this.sides = sides;
     
-    //rotates the array to the right so the sides faced there starting rotation
-    for(let i = this.rot;i > 0;i-=90){
-    	this.sides.unshift(this.sides.pop());
-    }
-  }
-  //rotates the peice 90 degrees. If negative eqauls true , rotates the pyramid -90 degrees
-  rotate90deg(bNegative){
+    		//rotates the array to the right so the sides faced there starting rotation
+   		 for(let i = this.rot;i > 0;i-=90){
+    			this.sides.unshift(this.sides.pop());
+    		 }
+  	}
+  	//rotates the peice 90 degrees. If negative eqauls true , rotates the Piece -90 degrees
+  	rotate90deg(bNegative){
   
-  	if(!bNegative){
-    //rotates the array to the right
-    	this.sides.unshift(this.sides.pop());
+  		if(!bNegative){
+    			//rotates the array to the right
+    			this.sides.unshift(this.sides.pop());
       
-      this.rot = (this.rot + 90) % 360;
-    }
+      			this.rot = (this.rot + 90) % 360;
+   		 }
     
-    else if(bNegative){
-    	//rotates the array to the left
-    	this.sides.push(this.sides.shift());
+    		else if(bNegative){
+    			//rotates the array to the left
+    			this.sides.push(this.sides.shift());
       
-      this.rot = this.rot === 0 ? 270 : this.rot - 90;
-    }
-    
-    //console.log(this.rot);
-    //console.log(this.sides);
-  }
+      			this.rot = this.rot === 0 ? 270 : this.rot - 90;
+    		}
+  	}
   
   
-  get x(){
-  	return this.coordX;
-  }
+  	get x(){
+  		return this.coordX;
+  	}
   
-  get y(){
-  	return this.coordY;
-  }
+  	get y(){
+  		return this.coordY;
+  	}
   
-  set x(increment){
-  	this.coordX += increment;
-  }
+  	set x(increment){
+  		this.coordX += increment;
+  	}
   
-  set y(increment){
-  	this.coordY += increment;
-  }
+  	set y(increment){
+  		this.coordY += increment;
+  	}
   
 }
