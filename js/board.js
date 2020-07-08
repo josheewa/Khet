f59c04652f63f13843f4b4a4d61459f1728da4ea
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-08 14:45:41 -0400

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,1 +1,20 @@
-
+class Board{
+	//configuration can be 1,2,3
+  //1 = Classic
+  //2 = IMHOTEP
+  //3 = Dynasty
+	constructor(configuration){
+  
+  	this.board = new Array(10);
+    this.board.fill(new Array(8));
+    
+  }
+  
+  classicalConfiguration(){
+  	
+  }
+  
+  
+  
+  
+}
