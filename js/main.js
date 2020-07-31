59e2c75b4b9ec3f353226867b42428fbc1929ee2
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-31 15:30:05 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 //"use strict"
+
+function main(){
 let yCoord1 = 9;
         
-var player1Tiles = document.getElementById("player1").querySelectorAll(".square");
-var player2Tiles = document.getElementById("player2").querySelectorAll(".square");
+        var player1Tiles = document.getElementById("player1").querySelectorAll(".square");
+        var player2Tiles = document.getElementById("player2").querySelectorAll(".square");
         
-player1Tiles.forEach((item,index) =>{
-    if(index % 10 === 0 ) yCoord1--;  
-    item.setAttribute('X', 9 - (index % 10)); 
-    item.setAttribute('Y', (yCoord1 % 9) - 1);   
-});
+        player1Tiles.forEach((item,index) =>{
+                if(index % 10 === 0 ) yCoord1--;  
+                item.setAttribute('X', 9 - (index % 10)); 
+                item.setAttribute('Y', (yCoord1 % 9) - 1);   
+        });
         
-yCoord1 = 0;
+        yCoord1 = 0;
 
         
-player2Tiles.forEach((item,index) =>{
-  if(index % 10 === 0 ) yCoord1++;  
-    item.setAttribute('X', index % 10); 
-    item.setAttribute('Y', (yCoord1 % 9) - 1);   
-});
+        player2Tiles.forEach((item,index) =>{
+                if(index % 10 === 0 ) yCoord1++;  
+                item.setAttribute('X', index % 10); 
+                item.setAttribute('Y', (yCoord1 % 9) - 1);   
+        });
 
-var khetBoard = new Board(1);
+        var khetBoard = new Board(1);
+        
+}
 
 
 
