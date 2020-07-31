22434cc61ee1d0dd1caa42c0a2a8a885a5bf39eb
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-31 15:22:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 "use strict"
-let yCoord = 9;
+let yCoord1 = 9;
         
 var player1Tiles = document.getElementById("player1").querySelectorAll(".square");
 var player2Tiles = document.getElementById("player2").querySelectorAll(".square");
         
 player1Tiles.forEach((item,index) =>{
-    if(index % 10 === 0 ) yCoord--;  
+    if(index % 10 === 0 ) yCoord1--;  
     item.setAttribute('X', 9 - (index % 10)); 
-    item.setAttribute('Y', (yCoord % 9) - 1);   
+    item.setAttribute('Y', (yCoord1 % 9) - 1);   
 });
         
 yCoord = 0;
 
         
 player2Tiles.forEach((item,index) =>{
-  if(index % 10 === 0 ) yCoord++;  
+  if(index % 10 === 0 ) yCoord1++;  
     item.setAttribute('X', index % 10); 
-    item.setAttribute('Y', (yCoord % 9) - 1);   
+    item.setAttribute('Y', (yCoord1 % 9) - 1);   
 });
 
 var khetBoard = new Board(1);
