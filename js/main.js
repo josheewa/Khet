1aaf1f9b1c83a2e72954780c2ff512159334c36a
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-31 15:45:15 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ let yCoord1 = 9;
         var player1 = document.getElementById("player1");
         var player2 = document.getElementById("player2");
         
-        var player1Tiles = player2.querySelectorAll(".square");
-        var player2Tiles = player2.querySelectorAll(".square");
+        var player1Tiles = player2.getElementsByClassName("square");
+        var player2Tiles = player2.getElementsByClassName("square");
         
         player1Tiles.forEach((item,index) =>{
                 if(index % 10 === 0 ) yCoord1--;  
