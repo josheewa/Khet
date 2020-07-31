b30244415112986b9a7e3e82b5e0ef4749c38542
seamoose44499958 <45863839+seamoose44499958@users.noreply.github.com>
2020-07-31 15:49:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 function main(){
 let yCoord1 = 9;
         
-        var player1 = document.getElementById("player1");
+        //var player1 = document.getElementById("player1");
         var player2 = document.getElementById("player2");
         
         var player1Tiles = player1.getElementsByTagName("div");
         var player2Tiles = player2.getElementsByClassName("square");
         
-        player1Tiles.forEach((item,index) =>{
+        /*player1Tiles.forEach((item,index) =>{
                 if(index % 10 === 0 ) yCoord1--;  
                 item.setAttribute('X', 9 - (index % 10)); 
                 item.setAttribute('Y', (yCoord1 % 9) - 1);   
-        });
+        });*/
         
         yCoord1 = 0;
 
